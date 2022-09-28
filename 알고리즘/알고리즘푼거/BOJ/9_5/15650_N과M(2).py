46c64fb98fe7e0713e8ddc5c13d7833fd2b2f8e6
soheemoon37 <tnghgml37@gmail.com>
2022-09-28 22:10:32 +0900

    220928

---
+++ 알고리즘/알고리즘푼거/BOJ/9_5/15650_N과M(2).py
@@ -0,0 +1,27 @@
+
+def perm(i, k, r):
+    if i == r:
+        for i in range(len(p)-1):
+            if p[i] < p[i+1]:
+                continue
+            else:
+                return
+        else:
+            for i in p:
+                print(i, end=' ')
+            print()
+
+
+    else:
+        for j in range(k):
+            if used[j] == 0:
+                used[j] = 1
+                p[i] = a[j]
+                perm(i+1, k, r)
+                used[j] = 0
+
+N, R = map(int, input().split())
+a = list(range(1, N+1))
+used = [0] * N
+p = [0] * R
+perm(0, N, R)
